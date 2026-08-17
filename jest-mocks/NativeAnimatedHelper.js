29ae2ed1650ec1529b460861b87beb8ee261304d
Hashir <historyhub71@gmail.com>
2026-08-17 02:40:56 -0700

    jest upload

---
+++ jest-mocks/NativeAnimatedHelper.js
@@ -0,0 +1,4 @@
+module.exports = {
+  addWhitelistedStyleProp: () => {},
+  addWhitelistedNativeProps: () => {},
+};
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  setupFilesAfterEnv: ['./jest-setup.js'],
+  moduleNameMapper: {
+    '^react-native/Libraries/Animated/NativeAnimatedHelper$': '<rootDir>/jest-mocks/NativeAnimatedHelper.js',
+    'react-native-worklets': '<rootDir>/jest-mocks/react-native-worklets.js',
+    '^react-native-reanimated/scripts/validate-worklets-version$': '<rootDir>/jest-mocks/validate-worklets-version.js',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|react-native-.*|@react-navigation-.*|react-native-sweet-alert|@react-native-async-storage)/)',
+  ],
 };
